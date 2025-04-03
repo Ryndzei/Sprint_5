4353de46ce33a81d92bd95d1c3aafeaa0b07ebaa
Anton Rineyskiy <arineyskiy@yandex.ru>
2025-04-03 22:46:43 +0300

    test_data.py added for sorting valid login data.

--- tests/test_go_from_personal_account.py
+++ tests/test_go_from_personal_account.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from urls import BASE_URL
+from test_data import *
 from locators import (
     LOGIN_EMAIL_INPUT, LOGIN_PASSWORD_INPUT,
     LOGIN_BUTTON, LOGIN_ACCOUNT_BUTTON,
@@ -13,8 +14,8 @@ def test_successfully_went_to_main_page_by_clicking_constructor(driver):
     driver.get(BASE_URL)
 
     driver.find_element(*LOGIN_ACCOUNT_BUTTON).click()
-    driver.find_element(*LOGIN_EMAIL_INPUT).send_keys("antonrineyskiy1939@yandex.ru")
-    driver.find_element(*LOGIN_PASSWORD_INPUT).send_keys("123456")
+    driver.find_element(*LOGIN_EMAIL_INPUT).send_keys(VALID_LOGIN)
+    driver.find_element(*LOGIN_PASSWORD_INPUT).send_keys(VALID_PASSWORD)
     driver.find_element(*LOGIN_BUTTON).click()
 
     WebDriverWait(driver, 3).until(expected_conditions.visibility_of_element_located(PERSONAL_CABINET_LINK))
@@ -31,8 +32,8 @@ def test_successfully_went_to_main_page_by_clicking_logo(driver):
     driver.get(BASE_URL)
 
     driver.find_element(*LOGIN_ACCOUNT_BUTTON).click()
-    driver.find_element(*LOGIN_EMAIL_INPUT).send_keys("antonrineyskiy1939@yandex.ru")
-    driver.find_element(*LOGIN_PASSWORD_INPUT).send_keys("123456")
+    driver.find_element(*LOGIN_EMAIL_INPUT).send_keys(VALID_LOGIN)
+    driver.find_element(*LOGIN_PASSWORD_INPUT).send_keys(VALID_PASSWORD)
     driver.find_element(*LOGIN_BUTTON).click()
 
     WebDriverWait(driver, 3).until(expected_conditions.visibility_of_element_located(PERSONAL_CABINET_LINK))
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from urls import BASE_URL, REGISTER_URL, PASS_FORGOT_URL
+from test_data import *
 from locators import *
 
 
@@ -8,8 +9,8 @@ def test_successful_login_from_main_page(driver):
     driver.get(BASE_URL)
 
     driver.find_element(*LOGIN_ACCOUNT_BUTTON).click()
-    driver.find_element(*LOGIN_EMAIL_INPUT).send_keys("antonrineyskiy1939@yandex.ru")
-    driver.find_element(*LOGIN_PASSWORD_INPUT).send_keys("123456")
+    driver.find_element(*LOGIN_EMAIL_INPUT).send_keys(VALID_LOGIN)
+    driver.find_element(*LOGIN_PASSWORD_INPUT).send_keys(VALID_PASSWORD)
     driver.find_element(*LOGIN_BUTTON).click()
 
     WebDriverWait(driver, 3).until(expected_conditions.visibility_of_element_located(PERSONAL_CABINET_LINK))
@@ -22,8 +23,8 @@ def test_successful_login_from_personal_cabinet(driver):
     driver.get(BASE_URL)
 
     driver.find_element(*PERSONAL_CABINET_LINK).click()
-    driver.find_element(*LOGIN_EMAIL_INPUT).send_keys("antonrineyskiy1939@yandex.ru")
-    driver.find_element(*LOGIN_PASSWORD_INPUT).send_keys("123456")
+    driver.find_element(*LOGIN_EMAIL_INPUT).send_keys(VALID_LOGIN)
+    driver.find_element(*LOGIN_PASSWORD_INPUT).send_keys(VALID_PASSWORD)
     driver.find_element(*LOGIN_BUTTON).click()
 
     WebDriverWait(driver, 3).until(expected_conditions.visibility_of_element_located(PERSONAL_CABINET_LINK))
@@ -38,8 +39,8 @@ def test_successful_login_from_registration_form(driver):
     driver.get(REGISTER_URL)
 
     driver.find_element(*LOGIN_FROM_REGISTER).click()
-    driver.find_element(*LOGIN_EMAIL_INPUT).send_keys("antonrineyskiy1939@yandex.ru")
-    driver.find_element(*LOGIN_PASSWORD_INPUT).send_keys("123456")
+    driver.find_element(*LOGIN_EMAIL_INPUT).send_keys(VALID_LOGIN)
+    driver.find_element(*LOGIN_PASSWORD_INPUT).send_keys(VALID_PASSWORD)
     driver.find_element(*LOGIN_BUTTON).click()
 
     WebDriverWait(driver, 3).until(expected_conditions.visibility_of_element_located(PERSONAL_CABINET_LINK))
@@ -52,8 +53,8 @@ def test_successful_login_from_password_recovery_form(driver):
     driver.get(PASS_FORGOT_URL)
 
     driver.find_element(*LOGIN_FROM_RECOVERY).click()
-    driver.find_element(*LOGIN_EMAIL_INPUT).send_keys("antonrineyskiy1939@yandex.ru")
-    driver.find_element(*LOGIN_PASSWORD_INPUT).send_keys("123456")
+    driver.find_element(*LOGIN_EMAIL_INPUT).send_keys(VALID_LOGIN)
+    driver.find_element(*LOGIN_PASSWORD_INPUT).send_keys(VALID_PASSWORD)
     driver.find_element(*LOGIN_BUTTON).click()
 
     WebDriverWait(driver, 3).until(expected_conditions.visibility_of_element_located(PERSONAL_CABINET_LINK))
